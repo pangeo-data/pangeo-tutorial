98d54e69d15acaaa08a97dde8d8469ca96dd5665
Sebastian Alvis <sebialvis@gmail.com>
2020-06-01 18:40:18 +0000

    Refactor utils.py to use requests library instead of wget

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,30 +1,35 @@
 
-import os
+import requests
+import shutil
 import tarfile
-import wget
+import os
 
-
-def get_sst_data(data_dir=None):
+def download_file(data_dir=None):
+    # Make data directory
     if data_dir is None:
         data_dir = os.path.join(os.path.expanduser('~'), '.xarray_tutorial_data')
-    
+
     os.makedirs(data_dir, exist_ok=True)
-
-    remote = 'http://ldeo.columbia.edu/~rpa/sst.tar.gz'
-
-    # download the data
-    filename = wget.download(remote, out=data_dir)
-
-    # un tar/zip the file
     cwd = os.getcwd()
     os.chdir(data_dir)
+    
+    # Data to download
+    url = 'http://ldeo.columbia.edu/~rpa/sst.tar.gz'
+    local_filename = url.split('/')[-1]
+    
+    # Download the data
+    with requests.get(url, stream=True) as r:
+        with open(local_filename, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+    
+    # un tar/zip the file
     try:
-        with tarfile.open(filename, "r:gz") as file:
+        with tarfile.open(local_filename, "r:gz") as file:
             file.extractall()
     finally:
         os.chdir(cwd)
 
     # remove tar.gz file
-    os.remove(filename)
-
+    os.remove(data_dir + '/' + local_filename)
+    
     print(f'\nsst data is in {data_dir}/sst')
